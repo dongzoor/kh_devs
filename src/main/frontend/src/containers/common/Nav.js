0ba3f8ff0c69dc33cd5f102e1a7828a26b70e77a
jaeyi <plotojjy@gmail.com>
2022-12-23 17:05:26 +0900

    J2 commit

--- src/main/frontend/src/containers/common/Nav.js
+++ src/main/frontend/src/containers/common/Nav.js
@@ -1,5 +1,6 @@
 import { getAuth, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
+
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -9,14 +10,15 @@ import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import LogoImg from "./image/devs.png";
+
+// import LogoImg from "./image/devs.png";
 
 const NAVI = styled.div`
   * {
     font-family: "Gowun Dodum", sans-serif;
 
-    .devs_nav{
-         font-size: 'bold';
+    .devs_nav {
+      font-size: "bold";
     }
   }
 `;
@@ -65,83 +67,97 @@ function OffcanvasExample() {
     <NAVI>
       {[false].map((expand) => (
         <>
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#" className="devs_nav">DEVS&nbsp;<img src={LogoImg} alt="" style={{  margin: '5px', width: '30px', height: '40px'}}/></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                &nbsp;DEVS&nbsp;<img src={LogoImg} alt="" style={{  margin: '5px', width: '30px', height: '40px'}}/>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  {isLogin !== "" && (
-                    <Nav.Link href="/" onClick={onClickLogout}>
-                      로그아웃
-                    </Nav.Link>
-                  )}
-                  <NavDropdown
-                    title="마이페이지"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
+          <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+            <Container fluid>
+              <Navbar.Brand href="#" className="devs_nav">
+                DEVS&nbsp;
+                <img
+                  // src={LogoImg}
+                  alt=""
+                  style={{ margin: "5px", width: "30px", height: "40px" }}
+                />
+              </Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls={`offcanvasNavbar-expand-${expand}`}
+              />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    &nbsp;DEVS&nbsp;
+                    <img
+                      // src={LogoImg}
+                      alt=""
+                      style={{ margin: "5px", width: "30px", height: "40px" }}
+                    />
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
                     {isLogin !== "" && (
-                      <NavDropdown.Item href="/user/check">
-                        내 정보 수정
-                      </NavDropdown.Item>
+                      <Nav.Link href="/" onClick={onClickLogout}>
+                        로그아웃
+                      </Nav.Link>
                     )}
-                    {isLogin !== "" && <NavDropdown.Divider />}
-                    {isLogin !== "" ? (
-                      <NavDropdown.Item href={`/myPage/myCalendar/${userId}`}>
-                        캘린더
-                      </NavDropdown.Item>
-                    ) : (
-                      <NavDropdown.Item href={"/"}>캘린더</NavDropdown.Item>
-                    )}
-                    {isLogin !== "" ? (
-                      <NavDropdown.Item href={`/myPage/myStudy/${userId}`}>
-                        나의 스터디
-                      </NavDropdown.Item>
-                    ) : (
-                      <NavDropdown.Item href={"/"}>
-                        나의 스터디
-                      </NavDropdown.Item>
-                    )}
+                    <NavDropdown
+                      title="마이페이지"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      {isLogin !== "" && (
+                        <NavDropdown.Item href="/user/check">
+                          내 정보 수정
+                        </NavDropdown.Item>
+                      )}
+                      {isLogin !== "" && <NavDropdown.Divider />}
+                      {isLogin !== "" ? (
+                        <NavDropdown.Item href={`/myPage/myCalendar/${userId}`}>
+                          캘린더
+                        </NavDropdown.Item>
+                      ) : (
+                        <NavDropdown.Item href={"/"}>캘린더</NavDropdown.Item>
+                      )}
+                      {isLogin !== "" ? (
+                        <NavDropdown.Item href={`/myPage/myStudy/${userId}`}>
+                          나의 스터디
+                        </NavDropdown.Item>
+                      ) : (
+                        <NavDropdown.Item href={"/"}>
+                          나의 스터디
+                        </NavDropdown.Item>
+                      )}
 
-                    {isLogin !== "" && <NavDropdown.Divider />}
-                    {isLogin !== "" && (
-                      <NavDropdown.Item href={`/myPage/mySocial/${userId}`}>
-                        My DevS
-                      </NavDropdown.Item>
+                      {isLogin !== "" && <NavDropdown.Divider />}
+                      {isLogin !== "" && (
+                        <NavDropdown.Item href={`/myPage/mySocial/${userId}`}>
+                          My DevS
+                        </NavDropdown.Item>
+                      )}
+                    </NavDropdown>
+                    <Nav.Link href="/studies">Study</Nav.Link>
+                    <Nav.Link href="/social">Social</Nav.Link>
+                    {localStorage.getItem("adminEmail") !== "" ? (
+                      <Nav.Link href="/AdminLogin">관리자모드</Nav.Link>
+                    ) : (
+                      <Nav.Link href="/AdminMemberList">관리자모드</Nav.Link>
                     )}
-                  </NavDropdown>
-                  <Nav.Link href="/studies">Study</Nav.Link>
-                  <Nav.Link href="/social">Social</Nav.Link>
-                  {localStorage.getItem("adminEmail") !== "" ? (
-                    <Nav.Link href="/AdminLogin">관리자모드</Nav.Link>
-                  ) : (
-                    <Nav.Link href="/AdminMemberList">관리자모드</Nav.Link>
-                  )}
-                  <Nav.Link href="/chat">Chat</Nav.Link>
-                </Nav>
-                <Form className="d-flex" style={{ marginTop: "20px" }}>
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+                    <Nav.Link href="/chat">Chat</Nav.Link>
+                  </Nav>
+                  <Form className="d-flex" style={{ marginTop: "20px" }}>
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
         </>
       ))}
     </NAVI>
